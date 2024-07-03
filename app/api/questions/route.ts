9e51c6467acfd08e019d6441b0900f92e436cc1e
gourink <gouri.nandakumar26@gmail.com>
2024-07-03 22:16:41 +0200

    Static route

--- app/api/questions/route.ts
+++ app/api/questions/route.ts
@@ -4,6 +4,13 @@ import { NextResponse } from 'next/server';
 export async function GET() {
   try {
     const questions = await prisma.question.findMany({
+      where : {
+        domain : {
+          value : {
+            contains : 'domain1'
+          }
+        }
+      },
       include: {
         domain: true,
       },
--- app/quiz/[domain]/test/page.tsx
+++ app/quiz/[domain]/test/page.tsx
@@ -1,10 +1,28 @@
-import QuestionSet from "@/app/ui/quiz/question-set";
+'use client'
 
-export default async function Page({ params }: { params: { domain: string } }) {
+import { QuestionType } from "@/app/lib/definitions";
+import QuestionSet from "@/app/ui/quiz/question-set";
+import { useEffect, useState } from "react";
+
+export default function Page({ params }: { params: { domain: string } }) {
     const domain = params.domain;
+
+    const [questionsForDomain, setQuestionsForDomain] = useState<QuestionType[]>([]);
+
+    useEffect(() => {
+      async function fetchQuestions() {
+        const response = await fetch('/api/questions');
+        const data = await response.json();
+        setQuestionsForDomain(data);
+      }
+  
+      fetchQuestions();
+    }, []);
+
     return (
       <div>
         Quiz Test Page {domain}
+        <br/>Fetched from DB {questionsForDomain.length}
         <QuestionSet type={domain}/>
         </div>
 
